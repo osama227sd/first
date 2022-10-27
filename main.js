fe87129996a0e9296353f87c5c180be2e90111b1
osama227sd <116837246+osama227sd@users.noreply.github.com>
2022-10-27 20:26:49 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+
+function phones(x){
+    
+    img.src=x;
+}
+
+
+function bgcolor(y){
+    
+ container.style.background = y;   
+}
